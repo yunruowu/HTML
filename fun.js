349c996395e8105075e8b6537bb93b4f2efc63f1
xuchen <mcdxwan@outlook.com>
2019-06-05 15:10:56 +0800

    贪吃蛇单机版完成

--- fun.js
+++ fun.js
@@ -1,10 +1,3 @@
-
-var w_i = 0;
-var w_j = 0;
-var a_i = 0;
-var a_j = 0;
-var s_i = 0;
-var s_j = 0;
 var d_i = 0;
 var d_j = 0;
 var destination = 'D';
@@ -14,13 +7,13 @@ function getspeed(){
     var index = level_d.selectedIndex;
     var val = level_d.options[index].value;
     if (val == "Easy"){
-        return 200;
+        return 20;
     }
     if (val == "Middle"){
         return 100;
 	}
     if (val == "Hard"){
-       	return 50;
+       	return 20;
     }
     else{
         return 400;
@@ -31,19 +24,46 @@ document.onkeydown =  function getDestination(evt){
     ekey = evt.keyCode;
     if (ekey==87){
         // console.log("wwww");
-        destination ='W';
+        if(destination =='S')
+        {
+            destination =='S'
+        }
+        else{
+            destination ='W';
+        }
     }
     if (ekey==65){
         // console.log("AAA");
-        destination ='A';
+        if(destination =='D')
+        {
+            destination =='D'
+        }
+        else{
+            destination ='A';
+        }
+        // destination ='A';
     }
     if (ekey==83){
         // console.log("SSS");
-        destination ='S';
+        if(destination =='W')
+        {
+            destination =='W'
+        }
+        else{
+            destination ='S';
+        }
+        // destination ='S';
     }
     if (ekey==68){
         // console.log("DDD");
-        destination= 'D';
+        if(destination =='A')
+        {
+            destination =='A'
+        }
+        else{
+            destination ='D';
+        }
+        // destination= 'D';
     }
     else{
         // console.log("mmmm");
@@ -53,47 +73,69 @@ document.onkeydown =  function getDestination(evt){
 function move(){
     var b = document.getElementById('snake'); 
     // console.log("1111");
+    var pre_i = d_i;
+    var pre_j = d_j;
     if (destination=='D'){
-        b.style.left = (d_i%24)*20+'px';
-        b.style.top = (d_j%16)*20+'px';
-        
-        if (d_i == 23)
-            d_j = (d_j+1)%16;   
-        d_i = (d_i+1)%24;
+        // b.style.left = (d_i%24)*20+'px';
+        // b.style.top = (d_j%16)*20+'px';
+        if (d_i == 23){
+            end();
+            alert("over");  
+        }else{
+            d_i = (d_i+1)%24;
+        }   
+       
     }
     if (destination=='S'){
-        b.style.left = (d_i%24)*20+'px';
-        b.style.top = (d_j%16)*20+'px';
+        // b.style.left = (d_i%24)*20+'px';
+        // b.style.top = (d_j%16)*20+'px';
         
-        if (d_j == 16)
-            d_i = (d_i+1)%24;   
-        d_j = (d_j+1)%16;
+        if (d_j == 15)        {
+            end();
+            alert("over");
+     
+        }else{
+            d_j = (d_j+1)%16;
+        } 
+        
     }
     if (destination=='A'){
-        b.style.left = (d_i%24)*20+'px';
-        b.style.top = (d_j%16)*20+'px';
-        
-        if (d_i == 23)
-            d_j = (d_j+1)%16;   
-        d_i = (d_i-1)%24+24;
+        // b.style.left = (d_i%24)*20+'px';
+        // b.style.top = (d_j%16)*20+'px';
+        if (d_i == 0){
+            end();
+            alert("over");  
+        }else{
+            d_i = (d_i-1)%24;
+        }   
+        // if (d_i == 23)
+        //     d_j = (d_j+1)%16;   
+        // d_i = (d_i-1)%24+24;
     }
     if (destination=='W'){
-        b.style.left = (d_i%24)*20+'px';
-        b.style.top = (d_j%16)*20+'px';
+        if (d_j == 0){
+            end();
+            alert("over");
+        }else{
+            d_j = (d_j-1)%16;
+        } 
         
-        if (d_j == 16)
-            d_i = (d_i+1)%24;   
-        d_j = (d_j-1)%16+16;
+        
         // console.log(d_j);
     }
+    b.style.left = (d_i%24)*20+'px';
+    b.style.top = (d_j%16)*20+'px';
     var food_d = document.getElementById('food');
-    f_j = food_d.style.left;
-    f_i = food_d.style.top;
+    f_i = food_d.style.left;
+    f_j = food_d.style.top;
     // console.log(b);
     console.log(food_d.style.left);
-    if((d_j%16)*20+'px'==f_j){
-        if((d_i%24)*20+'px'==f_i){
-            console.log("chi");
+    if((pre_j%16)*20+'px'==f_j){
+        if((pre_i%24)*20+'px'==f_i){
+            food_d.style.left = Math.ceil(((Math.random()*24)%24)-1)*20+'px';
+            food_d.style.top = Math.ceil(((Math.random()*16)%16)-1)*20+'px';
+            console.log("dsdsd");
+            console.log(food_d.style.left);
         }
     }
 
@@ -101,6 +143,7 @@ function move(){
 
 
 }
+var sta_or_end = 0;
 function start(){
     // var level_d = document.getElementById('seltable');
     // var index = levelu_d.selectedIndex;
@@ -114,10 +157,18 @@ function start(){
     // if (val == "Hard"){
     //     startVar = window.setInterval("move()",50);
         // }
-        
-    startVar = window.setInterval("move()",getspeed());
+    if(sta_or_end==0)
+    {
+        startVar = window.setInterval("move()",getspeed());
+        sta_or_end = 1;
+    }
 }
 function end(){
-    clearInterval(startVar);
+    if(sta_or_end==1){
+
+        clearInterval(startVar);
+        sta_or_end = 0;
+    }
+    
 }
 
