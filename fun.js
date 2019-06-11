4d23d9c4f9020b0113441e8c8b5be8a97090c05d
xuchen <mcdxwan@outlook.com>
2019-06-11 20:11:43 +0800

    注释

--- fun.js
+++ fun.js
@@ -1,15 +1,21 @@
+// 蛇头的相对坐标
 var d_i = 0;
 var d_j = 0;
+// 初始化方向
 var destination = 'D';
+// 玩家得分
 var score_play1 = 0;
+// 控制按键的次数
 var key_num = 0;
+// 控制速度提升
 var changeboll = 1;
-// var start = 0;
+var ekey = 87;
+
+// 用于获取当前速度，实现最初的难度选择和运行过程中的速度提升
 function getspeed(){
 	var level_d = document.getElementById('seltable');
     var index = level_d.selectedIndex;
     var val = level_d.options[index].value;
-  
     var speed = 0;
     if(val != null)
     {    if (val == "Easy"){
@@ -24,24 +30,22 @@ function getspeed(){
         }
     }
     else{
-        speed = 40;
-       
+        speed = 40;  
     }
-
+// 游戏过程中加速
     if((speed - score_play1/5*20)>=10){
         speed = speed - score_play1/5*20;
-        
         return speed;
     }else{
         return 10;
     }
 }
-var ekey = 87;
+
+// 获取键盘事件并解析具体内容
 document.onkeydown =  function getDestination(evt){
     ekey = evt.keyCode;
     if(key_num==0){
         if (ekey==87){
-            // console.log("wwww");
             if(destination =='S')
             {
                 key_num = 0;
@@ -52,7 +56,6 @@ document.onkeydown =  function getDestination(evt){
             }
         }
         if (ekey==65){
-            // console.log("AAA");
             if(destination =='D')
             {
                 destination =='D';
@@ -61,10 +64,8 @@ document.onkeydown =  function getDestination(evt){
             else{
                 destination ='A';
             }
-            // destination ='A';
         }
         if (ekey==83){
-            // console.log("SSS");
             if(destination =='W')
             {
                 destination =='W';
@@ -74,10 +75,8 @@ document.onkeydown =  function getDestination(evt){
                 destination ='S';
 
             }
-            // destination ='S';
         }
         if (ekey==68){
-            // console.log("DDD");
             if(destination =='A')
             {
                 destination =='A';
@@ -86,21 +85,17 @@ document.onkeydown =  function getDestination(evt){
             else{
                 destination ='D';
             }
-            // destination= 'D';
         }
         key_num++;
     }
     else{
-        // console.log("mmmm");
         destination = destination;
     }
 }
 
+// 获取蛇尾
 function tail_pos(head){
     var b = document.getElementsByClassName("body");
-    // console.log(b);
-    // var i =0 ;
-   // console.log(b.length);
     var tail =[];
     if(b.length == 0){
         tail[0]=head.style.top;
@@ -114,11 +109,10 @@ function tail_pos(head){
     return tail;
 }
 
+// 控制蛇身的移动
 function body(head){
     var b = document.getElementsByClassName("body");
-    // console.log(b);
     var i =b.length-1;
-    // var tail =[];
     for(;i>=0;i=i-1)
     {
         if (i==0)
@@ -132,7 +126,7 @@ function body(head){
     }
 }
 
-
+// 判断蛇是否吃到自己身体
 function hit_body(head){
     var b = document.getElementsByClassName("body");
     var i = head.style.left;
@@ -150,22 +144,15 @@ function hit_body(head){
     }
 }
 
+// 控制蛇头的移动以及判断是否吃食物，生成下一个食物
 function move(){
-    
     var b = document.getElementById('snake'); 
-    // console.log("1111");
     var pre_i = d_i;
     var pre_j = d_j;
     var snakenode =[];
     var tail = tail_pos(b);
-   // console.log("ssssssssssss");
-  //  console.log(tail);
-    
-   // console.log(tail);
     if (destination=='D'){
-        // b.style.left = (d_i%24)*20+'px';
-        // b.style.top = (d_j%16)*20+'px';
-        if (d_i == 23){
+        if (d_i == 23){//判断是否撞墙
             end();
             alert("over");  
         }else{
@@ -173,48 +160,35 @@ function move(){
         }   
     }
     if (destination=='S'){
-        // b.style.left = (d_i%24)*20+'px';
-        // b.style.top = (d_j%16)*20+'px';
-        
-        if (d_j == 15)        {
+        if (d_j == 15){//判断是否撞墙
             end();
             alert("over");
-     
         }else{
             d_j = (d_j+1)%16;
         } 
         
     }
     if (destination=='A'){
-        // b.style.left = (d_i%24)*20+'px';
-        // b.style.top = (d_j%16)*20+'px';
-        if (d_i == 0){
+        if (d_i == 0){//判断是否撞墙
             end();
             alert("over");  
         }else{
             d_i = (d_i-1)%24;
-        }   
-        // if (d_i == 23)
-        //     d_j = (d_j+1)%16;   
-        // d_i = (d_i-1)%24+24;
+        } 
     }
     if (destination=='W'){
-        if (d_j == 0){
+        if (d_j == 0){//判断是否撞墙
             end();
             alert("over");
         }else{
             d_j = (d_j-1)%16;
         } 
-        
-        
-        // console.log(d_j);
     }
 
     hit_body(b);
-
-
     body(b);
-    // body(b);
+    
+    // 实现蛇吃食物的动作，并随机刷新食物位置
     b.style.left = (d_i%24)*20+'px';
     b.style.top = (d_j%16)*20+'px';
     var food_d = document.getElementById('food');
@@ -222,20 +196,15 @@ function move(){
     
     f_i = food_d.style.left;
     f_j = food_d.style.top;
-    // console.log(b);
-    // console.log(food_d.style.left);
+
+    // 判断是否吃到食物
     if((pre_j%16)*20+'px'==f_j){
         if((pre_i%24)*20+'px'==f_i){
-            
-           // console.log(f_j,f_i);
-            
-            //console.log(tail);
             score_play1++;
             food_d.style.left = Math.ceil(((Math.random()*24)%24)-1)*20+'px';
             food_d.style.top = Math.ceil(((Math.random()*16)%16)-1)*20+'px';
-           // console.log("dsdsd");
-           // console.log(food_d.style.left);
             snakenode.push(food_d);
+            // 将食物“转化”为身体
             var newnode = document.createElement("div");
             newnode.id= "snake1";
             newnode.className = "body";
@@ -263,8 +232,9 @@ function move(){
     out1_play1.innerHTML = score_play1 + "分";
     key_num = 0;
 }
-var sta_or_end = 0;
+;
 
+// 实现虚拟按键
 function up(){
     if(destination =='S')
             {
@@ -306,24 +276,9 @@ function right(){
             }
 }
 
-
-
-
-
-
+var sta_or_end = 0;
+// 游戏开始
 function start(){
-    // var level_d = document.getElementById('seltable');
-    // var index = levelu_d.selectedIndex;
-    // var val = level_d.options[index].value;
-    // if (val == "Easy"){
-    //     startVar = window.setInterval("move()",200);
-    // }
-    // if (val == "Middle"){
-    //     startVar = window.setInterval("move()",100);
-    // }
-    // if (val == "Hard"){
-    //     startVar = window.setInterval("move()",50);
-        // }
     if(sta_or_end==0)
     {
         startVar = window.setInterval("move()",getspeed());
@@ -331,6 +286,8 @@ function start(){
         sta_or_end = 1;
     }
 }
+
+// 游戏结束
 function end(){
     if(sta_or_end==1){
         endtime();
@@ -341,6 +298,7 @@ function end(){
     
 }
 
+// 实现速度改变
 function changespeed()
 {
     end();
@@ -348,7 +306,7 @@ function changespeed()
 }
 
 
-
+// 设定计时器
 function settime(a){
     if(a<10)
         a = "0"+a;
@@ -363,7 +321,6 @@ function starttime(){
     var showm = settime(m);
     var shows = settime(s);
     document.getElementById("showtime").innerHTML=showh+":"+showm+":"+shows;
-
        s++;
     if(s == 60)
        {
@@ -374,7 +331,6 @@ function starttime(){
         m = 0;
         h++;
     }
-
     t = setTimeout("starttime()",1000);
 }
 
